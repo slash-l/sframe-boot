49f6335badc101de6b879dbd63db138dcd0c16c4
liujingyi <jingyi.liu@newtouch.cn>
2017-07-21 00:28:06 +0800

    统一异常处理框架 代码优化

--- boot-common/src/main/java/com/sframe/component/common/base/exception/BusinessException.java
+++ boot-common/src/main/java/com/sframe/component/common/base/exception/BusinessException.java
@@ -1,9 +1,5 @@
 package com.sframe.component.common.base.exception;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
-import org.springframework.stereotype.Component;
-
 import java.io.Serializable;
 
 /**
